0054bc05a08a42312fa39730c8b981af73ab4363
Daniil Steshenko <luciferburn26@gmail.com>
2015-11-14 14:39:42 +0300

    Async form handling

--- meadowlark.js
+++ meadowlark.js
@@ -96,11 +96,11 @@ app.get('/newsletter', function (req, res) {
 	res.render('newsletter', { csrf: 'CSRF token goes here' });
 });
 app.post('/process', function(req, res) {
-	console.log('From (from querystring): ' + req.query.form);
-	console.log('CSRF token (from hidden form field): ' + req.body._csrf);
-	console.log('Name (from visible form field): ' + req.body.name);
-	console.log('Name (from visible form field): ' + req.body.email);
-	res.redirect(303, '/thank-you');
+	if(req.xhr || req.accepts('json,html') === 'json') {
+		res.send({ success: true });
+	} else {
+		res.redirect(303, '/thank-you');
+	}
 });
 app.get('/thank-you', function (req, res) {
 	res.render('thank-you');
