649f6083de221e1dfae5125f69504e19c219cd71
Daniil Steshenko <luciferburn26@gmail.com>
2015-11-14 14:14:48 +0300

    Client/Server templating with handlebars and form handling workflow

--- meadowlark.js
+++ meadowlark.js
@@ -4,8 +4,45 @@ var fortune =require('./lib/fortune.js');
 var app = express();
 
 
+function getWeatherData(){
+	return {
+		locations: [
+			{
+				name: 'Portland',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+				weather: 'Overcast',
+				temp: '54.1 F (12.3 C)',
+			},
+			{
+				name: 'Bend',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+				weather: 'Partly Cloudy',
+				temp: '55.0 F (12.8 C)',
+			},
+			{
+				name: 'Manzanita',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+				weather: 'Light Rain',
+				temp: '55.0 F (12.8 C)',
+			},
+		],
+	};
+}
+
 var handlebars = require('express3-handlebars')
-				 .create({defaultLayout:'main'});
+				 .create({
+				 	defaultLayout:'main',
+				 	helpers: {
+				 		section: function(name, options) {
+				 			if(!this._sections) this._sections = {};
+				 			this._sections[name] = options.fn(this);
+				 			return null;
+				 		}
+				 	}
+				});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -13,8 +50,16 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(require('body-parser')());
+
 app.use(function (req, res, next) {
 	res.locals.showTests = app.get('env') !== 'production' && req.query.test == '1';
+	next();
+});
+
+app.use(function(req, res, next){
+	if(!res.locals.partials) res.locals.partials = {};
+	res.locals.partials.weather = getWeatherData();
 	next();
 });
 
@@ -36,6 +81,32 @@ app.get('/tours/oregon-coast', function(req, res){
 app.get('/tours/request-group-rate', function(req, res){
 	res.render('tours/request-group-rate');
 });
+app.get('/nursery-rhyme', function(req, res) {
+	res.render('nursery-rhyme');
+});
+app.get('/data/nursery-rhyme', function(req, res) {
+	res.json({
+		animal: 'squirrel',
+		bodyPart: 'tail',
+		adjective: 'bushy',
+		noun: 'heck',
+	});
+});
+app.get('/newsletter', function (req, res) {
+	res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+app.post('/process', function(req, res) {
+	console.log('From (from querystring): ' + req.query.form);
+	console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+	console.log('Name (from visible form field): ' + req.body.name);
+	console.log('Name (from visible form field): ' + req.body.email);
+	res.redirect(303, '/thank-you');
+});
+app.get('/thank-you', function (req, res) {
+	res.render('thank-you');
+});
+
+
 
 // custom 404 page
 app.use(function (req, res, next) {
