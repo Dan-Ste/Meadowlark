fd0ca8af51adb2a02da4e735a0361a62aa3bf8ca
Daniil Steshenko <luciferburn26@gmail.com>
2015-11-14 16:44:20 +0300

    File upload with formidable

--- meadowlark.js
+++ meadowlark.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var fortune =require('./lib/fortune.js');
+var express = require('express'),
+	fortune = require('./lib/fortune.js'),
+	formidable = require('formidable'),
+	bodyParser = require('body-parser');
 
 var app = express();
-
 
 function getWeatherData(){
 	return {
@@ -50,7 +51,8 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(require('body-parser')());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
 	res.locals.showTests = app.get('env') !== 'production' && req.query.test == '1';
@@ -95,6 +97,7 @@ app.get('/data/nursery-rhyme', function(req, res) {
 app.get('/newsletter', function (req, res) {
 	res.render('newsletter', { csrf: 'CSRF token goes here' });
 });
+
 app.post('/process', function(req, res) {
 	if(req.xhr || req.accepts('json,html') === 'json') {
 		res.send({ success: true });
@@ -102,9 +105,34 @@ app.post('/process', function(req, res) {
 		res.redirect(303, '/thank-you');
 	}
 });
+app.get('/contest/vacation-photo',function(req,res){
+	var now = new Date();
+	res.render('contest/vacation-photo',{
+		year: now.getFullYear(), 
+		month: now.getMonth()
+	});
+});
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, files){
+		if(err) return res.redirect(303, '/error');
+		console.log('received fields:');
+		console.log(fields);
+		console.log('received files:');
+		console.log(files);
+		res.redirect(303, '/thank-you');
+	});
+});
+
 app.get('/thank-you', function (req, res) {
 	res.render('thank-you');
 });
+app.get('/error', function (req, res) {
+	res.render('error');
+});
+
+
 
 
 
